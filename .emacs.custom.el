2c7dd0fa3ddaaf30c3caeea6e4bc0dd8a1546b39
boring-nerd-dot-py <nunesteixeiradaniel12@gmail.com>
2025-02-24 20:36:33 -0300

    REALLY QUICK COMMIT BECAUSE I HAVE TO GO

---
+++ .emacs.custom.el
@@ -0,0 +1,20 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(ef-autumn))
+ '(custom-safe-themes
+   '("b41d0a9413fb0034cea34eb8c9f89f6e243bdd76bccecf8292eb1fefa42eaf0a" "d6b369a3f09f34cdbaed93eeefcc6a0e05e135d187252e01b0031559b1671e97" "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac" "36c5acdaf85dda0dad1dd3ad643aacd478fb967960ee1f83981d160c52b3c8ac" "00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729" "aff0396925324838889f011fd3f5a0b91652b88f5fd0611f7b10021cc76f9e09" "90185f1d8362727f2aeac7a3d67d3aec789f55c10bb47dada4eefb2e14aa5d01" "64b3efc524a8e9e4e9006e60775e6ebc393abc0d47139b316ef12c706a386047" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" "ad7d874d137291e09fe2963babc33d381d087fa14928cb9d34350b67b6556b6d" "ca1b398ceb1b61709197478dc7f705b8337a0a9631e399948e643520c5557382" "75eef60308d7328ed14fa27002e85de255c2342e73275173a14ed3aa1643d545" "377bf88f6a5c5085bccdd96beeade170a55df7944e9768f1108ada15d4e71e02" "7524429b9671ca0ad0e281dc865717f44f358e417c7b587b57772430fcbe552f" "01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
+ '(package-selected-packages
+   '(frame-tabs lua-mode multiple-cursors github-dark-dimmed-theme use-package autothemer orderless rainbow-delimiters glsl-mode tabbar powerline nerd-icons melancholy-theme magit gruber-darker-theme google catppuccin-theme badwolf-theme autumn-light-theme atom-one-dark-theme atom-dark-theme all-the-icons ace-window ac-c-headers abyss-theme))
+ '(warning-suppress-log-types '((comp) (comp)))
+ '(warning-suppress-types '((use-package) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
